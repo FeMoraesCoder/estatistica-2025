16e589abec9fb87215d6897fa7a5a44fee460886
Felipe de Moraes <felipedemoraes.cursos@gmail.com>
2025-09-09 18:10:55 -0300

    Feat: Aulas de Frequência

--- freq_categoria.sql
+++ freq_categoria.sql
@@ -4,19 +4,22 @@ WITH tb_freq_abs AS (
         count(idTransacao) AS FreqAbs
 
     FROM points
+
     GROUP BY descCategoriaProduto
+
 ),
 
 tb_freq_abs_cum AS (
 
     SELECT *,
             sum(FreqAbs) OVER (ORDER BY descCategoriaProduto) AS FreqAbsAcum,
-            1.0 * FreqAbs / (SELECT sum(FreqAbs) FROM tb_freq_abs) AS FreqRelativa
+            100.0 * FreqAbs / (SELECT sum(FreqAbs) FROM tb_freq_abs) AS FreqRelativa
 
     FROM tb_freq_abs
+
 )
 
 SELECT *,
-       sum(FreqRelativa) OVER (ORDER BY descCategoriaProduto) AS FreqRelativaAcum
+        sum(FreqRelativa) OVER (ORDER BY descCategoriaProduto) AS FreqRelativaAcum
 
 FROM tb_freq_abs_cum
---
+++ tb_freq.py
@@ -0,0 +1,37 @@
+# %%
+
+import pandas as pd
+import sqlalchemy
+
+df = pd.read_csv("data/points_tmw.csv")
+df.head()
+
+engine = sqlalchemy.create_engine("sqlite:///data/tmw.db")
+
+df.to_sql("points", engine, if_exists="replace", index=False)
+# %%
+
+freq_produto = df.groupby(["descProduto"])[["idTransacao"]].count()
+
+freq_produto["Freq. Abs Acum."] = freq_produto["idTransacao"].cumsum()
+
+freq_produto["Freq. Relativa"] = freq_produto["idTransacao"] / freq_produto["idTransacao"].sum()
+
+freq_produto["Freq. Rel. Acum. "] = freq_produto["Freq. Relativa"].cumsum()
+
+freq_produto
+# %%
+
+freq_categoria = (df.groupby(['descCategoriaProduto'])[['idTransacao']]
+                    .count()
+                    .rename(columns={"idTransacao": "Freq. Abs."}))
+
+freq_categoria ['Freq. Abs Acum.'] = freq_categoria['Freq. Abs.'].cumsum()
+
+freq_categoria['Freq. Relat.'] = freq_categoria['Freq. Abs.'] / freq_categoria["Freq. Abs."].sum()
+
+freq_categoria['Freq. Relat. Acum.'] = freq_categoria['Freq. Relat.'].cumsum()
+
+freq_categoria
+
+# %%
